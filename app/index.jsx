e00cbe64953c48f4df2a7cf0174267529d428fd3
Umut Yenikardeşler <umutyenikardesler@gmail.com>
2024-12-10 17:16:24 +0300

    CRUD App 11.00

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
---
+++ data/todos.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+        "id": 1,
+        "title": "Finish reading the book",
+        "compleeted": true
+    },
+    {
+        "id": 2,
+        "title": "Buy groceries",
+        "compleeted": false
+    },
+    {
+        "id": 3,
+        "title": "Finish reading the book",
+        "compleeted": true
+    },
+    {
+        "id": 4,
+        "title": "Write a blog post",
+        "compleeted": true
+    },
+    {
+        "id": 5,
+        "title": "Clean the house",
+        "compleeted": false
+    }
+]

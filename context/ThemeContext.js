e7e99dfff9e59b56e6c8f36e42af6b0afb656a82
Umut Yenikardeşler <umutyenikardesler@gmail.com>
2024-12-23 23:36:10 +0300

    Lesson 6 Completed : React Native Data Storage, Theme Toggle, Animations & Fonts

---
+++ context/ThemeContext.js
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+import { Appearance } from "react-native";
+import { Colors } from '../contants/Colors';
+
+export const ThemeContext = createContext({})
+
+export const ThemeProvider = ({ children }) => {
+    const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme())
+
+    const theme = colorScheme === 'dark'
+        ? Colors.dark
+        : Colors.light
+
+    return (
+        <ThemeContext.Provider value={{
+            colorScheme, setColorScheme, theme
+        }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+
